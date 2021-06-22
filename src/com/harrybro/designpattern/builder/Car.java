f3105f1cdd6b3af48f87feaa0839ee1a209ce771
Harry-Bro <likeharrybro@gmail.com>
2021-06-23 08:48:07 +0900

    feat: Builder pattern

---
+++ src/com/harrybro/designpattern/builder/Car.java
@@ -0,0 +1,44 @@
+package com.harrybro.designpattern.builder;
+
+public class Car {
+
+    private Long id;
+    private String name;
+
+    private Car(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static class Builder {
+
+        private Long id;
+        private String name;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this.id, this.name);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

01354aa7c772eb600c02527af4260089f26fbc4d
Harry-Bro <likeharrybro@gmail.com>
2021-06-25 15:03:13 +0900

    feat: Add Prototype Pattern

---
+++ src/com/harrybro/designpattern/prototype/Main.java
@@ -0,0 +1,25 @@
+package com.harrybro.designpattern.prototype;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Employee employee = new Employee();
+        employee.loadData();
+        List<String> originEmployees = employee.getEmployees();
+
+        Employee clone1 = (Employee) employee.clone();
+        List<String> copyEmployees1 = clone1.getEmployees();
+        copyEmployees1.remove(0);
+
+        Employee clone2 = (Employee) employee.clone();
+        List<String> copyEmployees2 = clone2.getEmployees();
+        copyEmployees2.add("Happy");
+
+        System.out.println("origin: " + originEmployees);
+        System.out.println("copy: " + copyEmployees1);
+        System.out.println("copy: " + copyEmployees2);
+    }
+
+}
